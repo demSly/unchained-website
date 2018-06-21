acfc4c25fb9c2d510ed196aa25410ccb01fecafb
Pascal Kaufmann <kaufmae@me.com>
2018-06-21 09:57:22 +0200

    random control panel text

---
+++ pages/control-panel.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import { compose } from 'recompose';
+import variables from '../styles/variables';
+import connectApollo from '../lib/connectApollo';
+import withRegion from '../lib/withRegion';
+import connectI18n from '../lib/connectI18n';
+import PageLayout from '../components/PageLayout';
+
+const Platform = ({ platform }) => (
+  <PageLayout title={platform.meta_title} metaDescription={platform.meta_description}>
+
+    <div className="wrap wrap--vertical-padding">
+      <section className="section">
+        <h1>Unchained Control Panel</h1>
+        <p>Our Unchained Control Panel is a sweet UI to all data management API's
+          that the unchained engine provides out of the box. We provide it to you through a
+          SaaS model starting at USD 49 a month.
+
+          Contact us now if you want to use the engine and our control panel before the open source release of unchained engine.
+        </p>
+      </section>
+
+      <section className="section">
+
+        <div>
+          <h2>Features</h2>
+          <div className="feature-set">
+            <h3>Users</h3>
+            <ul>
+              <li>Delete & edit users</li>
+              <li>Enroll new users</li>
+              <li>See user profiles</li>
+              <li>See user logs</li>
+            </ul>
+          </div>
+
+          <div className="feature-set">
+            <h3>Products</h3>
+            <ul>
+              <li>Manage Products</li>
+            </ul>
+          </div>
+
+          <div className="feature-set">
+            <h3>Assortments</h3>
+            <ul>
+              <li>Manage Assortments</li>
+            </ul>
+          </div>
+
+          <div className="feature-set">
+            <h3>Orders</h3>
+            <ul>
+              <li>Manage Orders</li>
+            </ul>
+          </div>
+
+          <div className="feature-set">
+            <h3>Logs</h3>
+            <ul>
+              <li>See rich application logs, allowing you to drill down by user, product or order</li>
+            </ul>
+          </div>
+
+          <div className="feature-set">
+            <h3>Analytics</h3>
+            <ul>
+              <li>Observe key indices</li>
+            </ul>
+          </div>
+
+          <div className="feature-set">
+            <h3>More</h3>
+            <ul>
+              <li>Manage Countries</li>
+              <li>Manage Languages</li>
+              <li>Manage Payment Providers</li>
+              <li>Manage Delivery Providers</li>
+              <li>Manage Warehousing Providers</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <style jsx>{`
+      .advantages {
+        padding-bottom: 2em;
+      }
+      @media (min-width: 768px) {
+        .advantages {
+          display: flex;
+        }
+        .advantage {
+          display: flex;
+          justify-content: space-between;
+        }
+      }
+
+
+      .advantage {
+        margin-top: 2.5em;
+        padding: 2em 1.5em;
+        background-image: linear-gradient(to top left, ${variables.grayColor}, white);
+        background-size: 100%;
+        background-repeat: no-repeat;
+        width: 100%;
+        height: 90%;
+        box-shadow: 0 2px 8px rgba(0,0,0,.05), 0 4px 14px rgba(0,0,0,.1);
+      }
+
+      .advantage h2 {
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+
+      .advantage h3 {
+        margin-top: 0;
+      }
+
+
+      .advantage p {
+        margin-top: .25em;
+        margin-bottom: 0;
+      }
+
+      .advantage-img {
+        width: 120px;
+        height: 120px;
+        margin-right: .5em;
+      }
+
+      @media (min-width: 768px) {
+        .advantage p {
+          max-width: 80%;
+        }
+      }
+    `}
+    </style>
+
+  </PageLayout>
+);
+
+
+export default connectApollo(compose(
+  connectI18n,
+  withRegion('platform'),
+)(Platform));
--- pages/platform.js
+++ pages/platform.js
@@ -11,109 +11,23 @@ const Platform = ({ platform }) => (
 
     <div className="wrap wrap--vertical-padding">
       <section className="section">
-        <h1>The Unchained Commerce Platform</h1>
-        <p>To fully concentrate on the product development, unchained will not build storefronts and websites for sellers. We are going to build SaaS services to complement and fund the development of our open source software.</p>
+        <h1>Platform overview</h1>
         <h2>Unchained Engine</h2>
-        <h2>Managed Hosting</h2>
-        <p>soon</p>
+        <p>Open-source digital commerce engine...</p>
         <h2>Control Panel</h2>
+        <p>Our cloud-based admin ui to view, edit and analyze data on the unchained engine</p>
+        <h2>Professional Services</h2>
+        <p>Support, Training, Events, Partnerships, ...</p>
+        <h2>Managed Hosting</h2>
+        <p>Containerized Managed Hosting through Docker Swarm, bring your own datacenter ...</p>
       </section>
-
-      <section className="section">
-
-        <div>
-          <h2>Are we there yet? And where is this going?</h2>
-          <div className="feature-set">
-            <h3>Delivery</h3>
-            <ul>
-              <li>Downloadable Products</li>
-              <li>E-Mail the order to a third-party logistics provider&apos;s central inbox</li>
-              <li>Upload orders as CSV to a FTP host*</li>
-              <li>Transfer orders electronically to any other system</li>
-              <li>Post (Yellowcube), UPS, DHL Comissioning*</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>Documents</h3>
-            <ul>
-              <li>Generate PDF&apos;s locally through a local bookkeeping system
-                (SAP R/3, Abacus, ...)
-              </li>
-              <li>Generate PDF&apos;s through SaaS services (Smallinvoice)</li>
-              <li>Generate a new XML file for external processing</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>Messaging</h3>
-            <ul>
-              <li>Send Order Confirmations and Invoices by E-Mail, SMS or Whatsapp</li>
-              <li>Send User Registration Notifications via Slack</li>
-              <li>Twitter new orders anonymized*</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>Payments</h3>
-            <ul>
-              <li>Bitcoin, Ether & more cryptocurrencies</li>
-              <li>Invoice</li>
-              <li>Stripe</li>
-              <li>PostFinance</li>
-              <li>PayPal</li>
-              <li>Bonity-checked invoices (Deltavista)</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>Pricing</h3>
-            <ul>
-              <li>B2B Users special price</li>
-              <li>Date range dependent special prices</li>
-              <li>Dynamic tax rate retrieval</li>
-              <li>Product, Payment, Order or Delivery prices confiurable</li>
-              <li>Weather-dependent barbeque sausage price*</li>
-              <li>Order total weight dependent charges, dynamically calculated by UPS*</li>
-              <li>Prices that depend on Languages, countries, users, order quantities, ...*</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>Discounts</h3>
-            <ul>
-              <li>Christmas & Black Friday Discounts</li>
-              <li>Percentage reducing discount codes</li>
-              <li>Absolute amount reducing discount codes</li>
-              <li>Swear word dependent daily discounts based on Donald Trump Twitter feed*</li>
-              <li>Lottery or random prices: Every 1000th buyer gets the stuff free of charge*</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>Warehousing</h3>
-            <ul>
-              <li>ERP as Inventory Management*</li>
-              <li>Manage Inventory in Google Docs</li>
-              <li>Fire Just-in-time Production of any goods*</li>
-              <li>Real-time quantities (see an event get sold out live)*</li>
-            </ul>
-          </div>
-
-          <div className="feature-set">
-            <h3>And so much more</h3>
-            <ul>
-              <li>One shop for one world (Multi-country support)</li>
-              <li>Price your products in any currency or crypto-currency you like. $XLM maybe?</li>
-              <li>Multi-language support</li>
-              <li>Dynamic catalogs</li>
-            </ul>
-          </div>
-
-          <small className="mt3 db">* Examples of crazy plugins possible, built during projects.
-            Every plugin is implementable by a Junior Javascript developer in less than a day
-          </small>
-        </div>
+      <section>
+        Our Vision:
+        <blockquote cite="vision">
+          <p>
+            Become the next global market leader in digital commerce, unchaining enterprises from monolithic proprietary software.
+          </p>
+        </blockquote>
       </section>
     </div>
 
