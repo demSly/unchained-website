98690fef1228023625d07a1ea149cb66ce006f59
vedi <vedran@fliptation.ch>
2019-03-24 13:18:35 +0100

    add platform overview teaser to startpage

--- pages/index.js
+++ pages/index.js
@@ -216,6 +216,17 @@ Fits well with:
       </div>
     </div>
 
+    <div className="color-bg-secondary mb3">
+      <section className="wrap">
+        <div className="container text-center mt3">
+          <h4>Get an overview of our platform</h4>
+          <Link href="/platform">
+            <a className="button button--secondary mb3">Platform overview</a>
+          </Link>
+        </div>
+      </section>
+    </div>
+
     <section className="timeline wrap">
       <div className="container">
         <div className="timeline-item">
@@ -511,7 +522,6 @@ Token Generation Event
       .c-bg-dark {
         position: relative;
         z-index: 0;
-        margin-bottom: 3em;
         padding: 3em 0 4em;
       }
 
